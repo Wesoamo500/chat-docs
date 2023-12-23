ddd05c0e6b23d1b70104f2984646b36d199f4cb4
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-12-23 09:24:29 +0000

    ref: add sync feature for the first time users

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,6 +1,7 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/types/server'
 import {publicProcedure, router} from './trpc'
 import { TRPCError } from '@trpc/server';
+import { db } from '@/db';
 
 export const appRouter = router({
     authCallback: publicProcedure.query(async()=>{
@@ -8,7 +9,22 @@ export const appRouter = router({
 
         const user = await getUser();
 
-        if(!user!.id || !user!.email) throw new TRPCError({code: 'UNAUTHORIZED'})
+        if(!user?.id || !user?.email) throw new TRPCError({code: 'UNAUTHORIZED'})
+
+        const dbUser = await db.user.findFirst({
+            where:{
+                id: user?.id
+            }
+        })
+
+        if(!dbUser){
+            await db.user.create({
+                data:{
+                    id: user.id,
+                    email: user.email
+                }
+            })
+        }
 
         return {success: true}
     })
