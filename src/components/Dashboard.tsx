49b157018a011295203608ceb9032daed51e6675
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-12-24 00:39:12 +0000

    ref: add upload button component to the dashboard page

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,12 @@
-const Dashboard = () => {
-    return(
-        <h3>Hello World</h3>
-    )
-}
+import UploadBtn from "./UploadBtn";
 
-export default Dashboard
+const Dashboard = () => {
+  return <main className="mx-auto max-w-7xl md:p-10">
+    <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
+        <h1 className='mb-3 font-bold text-5xl text-gray-900'>My Docs</h1>
+        <UploadBtn/>
+    </div>
+  </main>;
+};
+
+export default Dashboard;
