316ef7d73e27b17be7372d99a6e0202a13bcfeaf
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-12-27 09:58:00 +0000

    ref: add toaster components to app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
 import { PropsWithChildren } from "react";
 import "react-loading-skeleton/dist/skeleton.css"
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
             inter.className
           )}
         >
+          <Toaster/>
           <Navbar />
           {children}
         </body>
